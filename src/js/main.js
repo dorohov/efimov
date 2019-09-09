49f4f22aa49dad2575cea810afd5baeaae862f4d
Pavel Proyavin <pavel@dorohovdesign.com>
2019-09-09 17:49:46 +0300

    cl f 090919

--- src/js/main.js
+++ src/js/main.js
@@ -43,6 +43,31 @@
             }
             $('html,body').animate({scrollTop: $(aid).offset().top - $('.navbar').innerHeight()},'slow');
         })
+
+        $('.sreviews__list__inner').slick({
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            prevArrow: '',
+            nextArrow: '',
+            responsive: [
+                {
+                    breakpoint: 1200,
+                    settings: {
+                        slidesToShow: 2,
+                        autoplay: true,
+                        slidesToScroll: 2
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        autoplay: true
+                    }
+                }
+            ]
+        })
         
     })
 })(jQuery);
